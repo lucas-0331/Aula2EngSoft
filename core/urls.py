4295c315283c1788637645590694a87a363e4004
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-03-04 00:47:41 -0300

    checkpoint

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 app_name = 'core'
 
 urlpatterns = [
-    path('', index),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('consumer/', TemplateView.as_view(template_name='consumer/index.html'), name='consumer_page'),
     path('kitchen/', TemplateView.as_view(template_name='kitchen/index.html'), name='kitchen_page'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def view(request):
+    return render(request, 'index.html')
